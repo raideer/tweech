a0be52a786dfea88c0389e59273ba25594f5609b
raideer <idmania1@gmail.com>
2015-10-12 22:20:54 +0300

    Using facades instead of container variables

--- app/bot.php
+++ app/bot.php
@@ -1,23 +1,22 @@
 <?php
-$client = $app['client'];
-
 /**
- * Run when the client has connected to the IRC server
+ * Gets executed when the client has connected to the IRC server
  */
-$client->whenLogged(function() use($client){
-
+Client::whenLogged(function()
+{
   /**
    * Join a channel
    */
-  $client->command("JOIN", "#lirik");
+
+  Client::command("JOIN", "#lirik");
 });
 
 /**
  * Listen to the 'irc.message.PRIVMSG' event
  * @var Raideer\Tweech\Event\IrcMessageEvent
  */
-$client->listen("irc.message.PRIVMSG", function($event){
-
+Client::listen("irc.message.PRIVMSG", function($event)
+{
     $message = $event->getMessage();
 
     echo $message['username'] . ": " . $message['message'] . "\n";
