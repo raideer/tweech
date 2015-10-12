6167a4dcc9a0e1751e516d6c21bc3ff602482cf6
Kristofers Ozolins <es@kristofers.xyz>
2015-10-12 13:29:53 +0300

    Completed the ChatCommand class

--- app/commands/ChatCommand.php
+++ app/commands/ChatCommand.php
@@ -1,5 +1,29 @@
 <?php
+use Raideer\Tweech\Command\CommandInterface;
 
-class ChatCommand extends \Raideer\Tweech\Command\Command{
+class Command implements CommandInterface{
+
+  /**
+   * Get the name of the command
+   * @return string Return the name of the chat command
+   */
+  public function getCommand(){
+
+    /**
+     * return "help";
+     * Will listen for !help or .help
+     */
+
+    return null;
+  }
+
+  /**
+   * Runs when the command has been typed in
+   * @param  Raideer\Tweech\Event\Event $message Returns the event with the details
+   * @return void
+   */
+  public function run(Raideer\Tweech\Event\Event $message){
+
+  }
 
 }
