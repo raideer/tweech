e5dca778bde83c33a60a3cbd94b2deb713511f59
Kristofers Ozolins <es@kristofers.xyz>
2015-10-13 15:48:17 +0300

    Added the config facade

--- config/facades.php
+++ config/facades.php
@@ -4,5 +4,5 @@ return array(
   'Application' => 'Raideer\Tweech\Facades\App',
   'Client' => 'Raideer\Tweech\Facades\Client',
   'Logger' => 'Raideer\Tweech\Facades\Logger',
-  'Config' => 'Raideer\Tweech\Facades\Config',
+  'Config' => 'Raideer\Tweech\Facades\Config'
 );
