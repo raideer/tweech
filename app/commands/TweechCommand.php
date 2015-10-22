a9fff289911298e8bf7f1b73b2264f9a809ae9cc
Kristofers Ozolins <es@kristofers.xyz>
2015-10-22 11:10:19 +0300

    Edited the command

--- app/commands/TweechCommand.php
+++ app/commands/TweechCommand.php
@@ -1,14 +1,24 @@
 <?php
 
-class TweechCommand extends ChatCommand{
+class TweechCommand implements \Raideer\Tweech\Command\CommandInterface{
 
-  public function getCommand(){
+  public function getCommandIdentifier()
+  {
+    return array('!');
+  }
 
+  public function getCommand()
+  {
     return "tweech";
   }
 
-  public function run($event){
-
-    echo "Hello";
+  public function run($event)
+  {
+    $message = remove_command_str($event->getMessage(), "!tweech");
+    if($message == "Kappa"){
+      $event->getChat()->message("Hello! Kappa");
+    }else{
+      $event->getChat()->message("Hello!");
+    }
   }
 }
