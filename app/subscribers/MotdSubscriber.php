96ce43178b68993da2ac31a0997b4bb20bed9726
raideer <idmania1@gmail.com>
2015-12-23 15:20:51 +0200

    Using square brackets instead of array()

--- app/subscribers/MotdSubscriber.php
+++ app/subscribers/MotdSubscriber.php
@@ -1,14 +1,19 @@
 <?php
 namespace App\Subscribers;
 
+/**
+ * This class is an example how Subscribers work and how to use them.
+ *
+ *
+ */
 class MotdSubscriber extends Subscriber{
 
   public static function getSubscribedEvents(){
-    return array(
-      "irc.message.RPL_MOTD" => array(
+    return [
+      "irc.message.RPL_MOTD" => [
         "receivedMessageOfTheDay", 0
-      )
-    );
+      ]
+    ];
   }
 
   public function receivedMessageOfTheDay($event){
