e0afd52521758148a427b54f2a52e535df9e8f69
raideer <idmania1@gmail.com>
2015-12-25 02:15:57 +0200

    Removed unecessary namespace

--- app/subscribers/Subscriber.php
+++ app/subscribers/Subscriber.php
@@ -3,12 +3,11 @@
 namespace App\Subscribers;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Raideer\Tweech\Subscribers\EventSubscriber;
 
 abstract class Subscriber implements EventSubscriberInterface{
 
   public static function getSubscribedEvents(){
     return [];
   }
-  
+
 }
