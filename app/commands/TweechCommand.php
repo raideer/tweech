df1ec7a14404e582dee3615cfb2769d61550c070
raideer <idmania1@gmail.com>
2016-02-25 21:25:53 +0200

    Fixed Stuff

--- app/commands/TweechCommand.php
+++ app/commands/TweechCommand.php
@@ -6,17 +6,17 @@ class TweechCommand implements \Raideer\Tweech\Command\CommandInterface
 {
     public function getCommand()
     {
-        return "tweech";
+        return 'tweech';
     }
 
     public function run($event)
     {
         $message = $event->getCommand();
 
-        if ($message == "Kappa") {
-            $event->getChat()->message("Hello! Kappa");
+        if ($message == 'Kappa') {
+            $event->getChat()->message('Hello! Kappa');
         } else {
-            $event->getChat()->message("Hello!");
+            $event->getChat()->message('Hello!');
         }
     }
 }
