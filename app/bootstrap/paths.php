b443069b3f7061fab30b885b812a43277937ca66
raideer <idmania1@gmail.com>
2016-02-25 17:24:22 +0200

    Added storage path

--- app/bootstrap/paths.php
+++ app/bootstrap/paths.php
@@ -1,11 +1,8 @@
 <?php
 
-return array(
-
+return [
   "base" => __DIR__ . "/../../",
-
   "app" => __DIR__ . "/../../app",
-
   "config" => __DIR__ . "/../../config",
-
-);
+  "storage" => __DIR__ . "/../storage",
+];
