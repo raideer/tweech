a6b0909b1437d228fd45501a4f413ae018fded11
raideer <idmania1@gmail.com>
2016-03-23 21:55:46 +0200

    Indentation

--- config/app.php
+++ config/app.php
@@ -1,52 +1,52 @@
 <?php
 
 return [
-  'timezone' => 'UTC',
+    'timezone' => 'UTC',
 
-  /*
-   * Creates a new log file every day
-   *
-   * If false then everything will be logged into a single file
-   */
-  'dailyLogs' => true,
+    /*
+    * Creates a new log file every day
+    *
+    * If false then everything will be logged into a single file
+    */
+    'dailyLogs' => true,
 
-  /*
-   * Specify the file amount limit for daily logs
-   *
-   * 0 for unlimited
-   */
-  'dailyLogLimit' => 0,
+    /*
+    * Specify the file amount limit for daily logs
+    *
+    * 0 for unlimited
+    */
+    'dailyLogLimit' => 0,
 
-  'connection' => [
-      /*
-       * Specify the twitch username for the bot
-       */
-      'username' => 'raideeeeer',
+    'connection' => [
+        /*
+        * Specify the twitch username for the bot
+        */
+        'username' => 'raideeeeer',
 
-      /*
-       * Specify the Twitch chat OAuth password
-       *
-       * You can get your token here: http://www.twitchapps.com/tmi
-       */
-      'oauth' => 'oauth:p5x5wptcvdhotshre5bpq48jjzyu02',
+        /*
+        * Specify the Twitch chat OAuth password
+        *
+        * You can get your token here: http://www.twitchapps.com/tmi
+        */
+        'oauth' => 'oauth:p5x5wptcvdhotshre5bpq48jjzyu02',
 
-      /*
-       * Irc server information
-       */
-      'ircServer' => [
+        /*
+        * Irc server information
+        */
+        'ircServer' => [
 
-        'hostname' => 'irc.chat.twitch.tv',
-        'port'     => 6667,
+            'hostname' => 'irc.chat.twitch.tv',
+            'port'     => 6667,
 
-      ],
-  ],
+        ],
+    ],
 
-  'facades' => [
-    'Api'           => \Raideer\Tweech\Facades\Api::class,
-    'Application'   => \Raideer\Tweech\Facades\App::class,
-    'Client'        => \Raideer\Tweech\Facades\Client::class,
-    'Config'        => \Raideer\Tweech\Facades\Config::class,
-    'Logger'        => \Raideer\Tweech\Facades\Logger::class,
-  ],
+    'facades' => [
+        'Api'           => \Raideer\Tweech\Facades\Api::class,
+        'Application'   => \Raideer\Tweech\Facades\App::class,
+        'Client'        => \Raideer\Tweech\Facades\Client::class,
+        'Config'        => \Raideer\Tweech\Facades\Config::class,
+        'Logger'        => \Raideer\Tweech\Facades\Logger::class,
+    ],
 
 ];
